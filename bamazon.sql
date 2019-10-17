18024a9231dca351117032e3ce58afa8eb05d888
Gerson Cabrera <ga.cabrera05@gmail.com>
2019-10-16 17:54:43 -0700

    added table and mock data

---
+++ bamazon.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS bamazon;
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products (
+    item_id INT NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(100) NOT NULL,
+    department_name VARCHAR(100) NOT NULL,
+    price INT NOT NULL,
+    stock_quantity INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+SELECT * FROM bamazon.products;
+INSERT INTO bamazon.products (product_name, department_name, price, stock_quantity)
+VALUES ("Dog", "Animals", 500, 5);
+
+INSERT INTO bamazon.products (product_name, department_name, price, stock_quantity)
+VALUES ("Cat", "Animals", 200, 20);
+
+INSERT INTO bamazon.products (product_name, department_name, price, stock_quantity)
+VALUES ("Beans", "Canned Goods", 0.68, 419);
+
+INSERT INTO bamazon.products (product_name, department_name, price, stock_quantity)
+VALUES ("Batman Comics", "Books & Comics", "3.99", 20);
+
+INSERT INTO bamazon.products (product_name, department_name, price, stock_quantity)
+VALUES ("50 Shades of John McCain", "Books & Comics", "20", 4);

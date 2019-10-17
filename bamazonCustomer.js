c1bc84098b01b97b4f4a34c3fd7fa98d67765867
Gerson Cabrera <ga.cabrera05@gmail.com>
2019-10-16 18:58:02 -0700

    added connection to bamazon database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,18 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
 var Table = require('cli-table');
+
+//creating connection information for the sql database
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "bamazon"
+});
+
+// connecting to mysql server and database
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("\nConnected. \nConnection ID " + connection.threadId + ". \n\nWelcome to Bamazon!\n\n")
+})

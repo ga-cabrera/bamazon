5364a5a0ccc533cf7767aff66c815c6df0a3aa98
Gerson Cabrera <ga.cabrera05@gmail.com>
2019-10-16 21:47:50 -0700

    added function that asks customer what they would like to purchase and how many using the inquirer node

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -21,18 +21,43 @@ connection.connect(function(err) {
 var displayItems = function() {
     connection.query("SELECT * FROM  bamazon.products", function(err, res) {
         if (err) throw err;
+        // variable creates a table in terminal/bash
         var tableDisplay = new Table ({
             head: ["Item ID", "Product Name", "Department", "Price", "Quantity"],
-            colWidths: [10,30, 25, 7, 14]
+            colWidths: [10,30, 25, 7, 14] //gives width of each column 
 
         });
+        // for loop that goes through every row in bamazon database and pushes to table
         for (var i = 0; i < res.length; i++) {
             tableDisplay.push(
                 [res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
             );
         }
         console.log(tableDisplay.toString());
-
+        selectItem()
     }
     )};
     displayItems();
+
+    //function that asks customer what they would like to purchase
+    function selectItem() {
+        inquirer
+            .prompt ([
+                {
+                    name: "selectItem",
+                    type: "input",
+                    message: "What would you like to buy? Please enter Item ID",
+                    filter: Number //only accepts numbers as an input
+                },
+                {
+                    name: "amount",
+                    type: "input",
+                    message: "How many would you like to buy?",
+                    filter: Number //only allows numbers as an input
+                }
+            ]).then(function(answers) {
+                var selectedID = answers.selectItem;
+                var amountNeeded = answers.amount;
+                purchaseItem(selectedID, amountNeeded);
+            });
+    };

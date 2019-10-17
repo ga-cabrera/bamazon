b29c0ae1cfcc48d90d77f5806fdce182eb219911
Gerson Cabrera <ga.cabrera05@gmail.com>
2019-10-16 21:21:58 -0700

    added function that displays table of listed items in terminal/bash

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,4 +15,24 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     console.log("\nConnected. \nConnection ID " + connection.threadId + ". \n\nWelcome to Bamazon!\n\n")
-})
+});
+
+//function which displays product via table node
+var displayItems = function() {
+    connection.query("SELECT * FROM  bamazon.products", function(err, res) {
+        if (err) throw err;
+        var tableDisplay = new Table ({
+            head: ["Item ID", "Product Name", "Department", "Price", "Quantity"],
+            colWidths: [10,30, 25, 7, 14]
+
+        });
+        for (var i = 0; i < res.length; i++) {
+            tableDisplay.push(
+                [res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
+            );
+        }
+        console.log(tableDisplay.toString());
+
+    }
+    )};
+    displayItems();
